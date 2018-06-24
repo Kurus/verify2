67303f154bc42888874f74dbd35a7fd008dac68b
kamalavasan <kkvasan@live.com>
2018-06-24 12:41:31 +0530

    few more files

--- same_input_out_stride.py
+++ same_input_out_stride.py
@@ -1,23 +1,29 @@
+# support stride 2
 import numpy as np
 from scipy import signal as sg
 dim = 12
 dim_p=dim + 2
-dep = 32
+dep = 64
 ker = 64
 sq_ker = 64
 pool_en = 0
 av_pool_en = 0
-random = 1 #TODO
+stride2_en = 1
+random = 0 #TODO
 sq_rep = 0 # repete squze kernl for last layer
 
 #######################         Input image
 in_l = np.zeros(dim_p*dim_p*dep, dtype='uint8').reshape((dim_p,dim_p,dep))
 if random == 0:
-    in_ori = np.arange(dim*dim*dep, dtype='uint8').reshape((dim,dim,dep))
+    in_ori = np.full(dim*dim*dep, 0, dtype='uint8').reshape((dim,dim,dep))
+    in_ori[:,:,0] = np.arange(dim*dim, dtype = 'uint8').reshape(dim,dim)
+    # in_ori = np.arange(dim*dim*dep, dtype='uint8').reshape((dim,dim,dep))
+    # in_ori = np.random.randint(low = 0, high = 255, size = (dim,dim,dep), dtype='uint8')
+    in_ori = in_ori%128
 else:
     in_ori = np.random.randint(low = 0, high = 255, size = (dim,dim,dep), dtype='uint8')
 in_l[1:-1,1:-1,:] = in_ori
-# print(in_l[:,:,0]); print("_____________________-")
+print("input layer");print(in_l[:,:,0]);
 f_in = open("input_layer.txt","w")
 f_in_b = open("input_layer.bin","wb")
 for z in range(0,dim):
@@ -36,10 +42,13 @@ for d in range(0,dep):
             lis = in_ori[z,y,d].flatten().tolist()
             f_in_c.write(str(lis)[1:-1]+'\n')
             f_in_c_b.write(bytearray(lis))
+if stride2_en==1:
+    in_l=in_ori
 ########################        expand kernels 
-# ker_l_1 =np.zeros(ker*dep, dtype='uint8').reshape((ker,dep))
-ker_l_1 =np.random.randint(100,size=ker*dep, dtype='uint8').reshape((ker,dep))
-# print(ker_l_1);print("________")
+ker_l_1 =np.zeros(ker*dep, dtype='uint8').reshape((ker,dep))
+ker_l_1[0,0]=1
+# ker_l_1 =np.random.randint(100,size=ker*dep, dtype='uint8').reshape((ker,dep))
+print("kernel1");print(ker_l_1)
 f_k_1 = open("ker_1x1.txt","w")
 f_k_1_b = open("ker_1x1.bin","wb")
 # for z in range(0,dep):
@@ -57,9 +66,11 @@ for z in range(0,dep):
         f_k_1.write(str(lis)[1:-1]+'\n')
 
 # ker_l_3 = np.asarray(list(range(0,9))*ker*dep, dtype='uint8').reshape((ker,dep,9))
+ker_l_3 = np.full(ker*dep*9, 0, dtype='uint8').reshape((ker,dep,9))
+ker_l_3[0,0,4]=1
 # ker_l_3 = np.ones(ker*dep*9, dtype='uint8').reshape((ker,dep,9))
-ker_l_3 = np.random.randint(100,size=ker*dep*9, dtype='uint8').reshape((ker,dep,9))
-# print(ker_l_3[0:8,0,:]);print("________")
+# ker_l_3 = np.random.randint(100,size=ker*dep*9, dtype='uint8').reshape((ker,dep,9))
+print("kernel3");print(ker_l_3[0:8,0,:]);
 f_k_3 = open("ker_3x3.txt","w")
 f_k_3_b = open("ker_3x3.bin","wb")
 # for m in range(0,dim): # repet 3x3 kernel # removed repeating
@@ -76,10 +87,11 @@ for z in range(0,dep):
         f_k_3.write(str(nin)[1:-1]+'\n')
 
 ########################        exapnd bias
-# bis_1 = np.arange(ker,dtype='uint8')
-bis_1 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
-# bis_3 = np.ones(ker,dtype='uint8')
-bis_3 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
+bis_1 = np.zeros(ker,dtype='uint8')
+#bis_1 = np.full(ker,1,dtype='uint8')
+# bis_1 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
+bis_3 = np.full(ker,0,dtype='uint8')
+# bis_3 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
 b_bis = open("bias.txt","w")
 b_bis_b = open("bias.bin","wb")
 # print(bis_1)
@@ -93,12 +105,14 @@ for i in range(0,ker,4):
 
 #######################        expand convolution
 out_1 = np.zeros(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
-for k in range(0,ker):
-    for l in range(0,dep):
-        res = sg.convolve(in_l[:,:,l],[[ker_l_1[k,l]]] , "valid").astype(int)
-        res = np.bitwise_and(res, 0xff)
-        out_1[k,l,:,:]=res[1:-1,1:-1]
-# print(out_1[1,1,:,:]);print('______')
+#stride 2 means 3x3 conv only
+if stride2_en==0:
+    for k in range(0,ker):
+        for l in range(0,dep):
+            res = sg.convolve(in_l[:,:,l],[[ker_l_1[k,l]]] , "valid").astype(int)
+            res = np.bitwise_and(res, 0xff)
+            out_1[k,l,:,:]=res[1:-1,1:-1]
+print("exp1 conv - no addition");print(out_1[0,0,:,:]);
 f_out_1 = open("out_1x1.txt","w")
 f_out_1_b = open("out_1x1.bin","wb")
 # out_1 = np.arange(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
@@ -110,14 +124,35 @@ for r in range(0,dim):
             f_out_1.write(str(lis)[1:-1]+'\n')
 
 
-out_3 = np.zeros(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
-for k in range(0,ker):
-    for l in range(0,dep):
-        kk = np.rot90(ker_l_3[k,l].reshape((3,3)),2)
-        res = sg.convolve(in_l[:,:,l],kk , "valid").astype(int) # addre lus
-        res = np.bitwise_and(res, 0xff)
-        out_3[k,l,:,:]=res
-# print(out_3[1,1,:,:]);print('______')
+if stride2_en==0:
+    out_3 = np.zeros(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
+    for k in range(0,ker):
+        for l in range(0,dep):
+            kk = np.rot90(ker_l_3[k,l].reshape((3,3)),2)
+            res = sg.convolve(in_l[:,:,l],kk , "valid").astype(int) # addre lus
+            res = np.bitwise_and(res, 0xff)
+            out_3[k,l,:,:]=res
+if stride2_en==1:
+    if dim%2==0:
+        o_dim = dim//2 - 1
+    else:
+        o_dim= dim //2
+    out_3 = np.zeros(ker*dep*o_dim*o_dim, dtype='uint8').reshape((ker,dep,o_dim,o_dim))
+    for k in range(0,ker):
+        for l in range(0,dep):
+            kk = ker_l_3[k,l]
+            for a in range(0,dim-2,2):
+                for b in range(0,dim-2,2):
+                    ll = in_l[a:a+3,b:b+3,l].flatten()
+                    out_3[k,l,a//2,b//2]=sum(np.multiply(kk,ll))
+    dim=o_dim
+
+print("exp 3 conv - no addtion")
+print("single layer")
+print(out_3[0,0,:,:])
+print("single pixel")
+print(out_3[0,:,0,0])
+# exit()
 # out_3 = np.arange(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
 
 f_out_3 = open("out_3x3.txt","w")
@@ -132,6 +167,9 @@ for r in range(0,dim):
 ############################ add bias and relu
 
 out_1 = np.sum(out_1,1,dtype='uint8') 
+print("exp1 after exp sinle pix")
+print(out_1[:,0,0])
+
 for i in range(0,ker):
     out_1[i,:,:] = out_1[i,:,:] + bis_1[i]
 out_1[out_1 > 127] = 0 # no need for positive
@@ -145,6 +183,11 @@ for x in range(0,dim):
 
 
 out_3 = np.sum(out_3,1,dtype='uint8')
+print("exp3 after exp sinle pix")
+print(out_3[:,0,0])
+print("exp3 after exp single layer")
+print(out_3[0,:,:])
+
 for i in range(0,ker):
     out_3[i,:,:] = out_3[i,:,:] + bis_3[i]
 out_3[out_3 > 127] = 0
@@ -155,18 +198,25 @@ for x in range(0,dim):
         lis=out_3[:,x,y]
         exp_out_3_b.write(bytearray(lis))
         exp_out_3.write(str(lis)[1:-1]+'\n')
-
+print("exp1 after bias sinle pix")
+print(out_1[:,0,0])
+print("exp1 after bias single layer")
+print(out_1[0,:,:])
+print("exp3 after bias sinle pix")
+print(out_3[:,0,0])
+print("exp3 after bias single layer")
+print(out_3[0,:,:])
 ############################# pooling
 dim_o = (dim - 1)//2
 # out_1 = np.arange(ker*dim*dim, dtype='uint8').reshape((ker,dim,dim)) #test pool
-# print(out_1)
 pool_1 = np.zeros((ker,dim_o,dim_o), dtype = 'uint8') #initialize
 for x in range(0,dim_o):
     xx = x*2
     for y in range(0,dim_o):
         yy = y*2
         pool_1[:,x,y]= np.amax(out_1[:,xx:xx+3,yy:yy+3],(1,2))
-# print(out_1[:,:,:]);print(pool_1[:,:,:]) # pool checking 
+print("pool 1")
+print(pool_1[0,:,:]) # pool checking 
 pool_out_1 = open("pool_1.txt","w")
 pool_out_1_b = open("pool_1.bin","wb")
 # print(pool_1)
@@ -175,7 +225,6 @@ for x in range(0,dim_o):
         lis=pool_1[:,x,y]
         pool_out_1_b.write(bytearray(lis))
         pool_out_1.write(str(lis)[1:-1]+'\n')
-
 # out_3 = np.arange(ker*dim*dim, dtype='uint8').reshape((ker,dim,dim)) #test pool
 # print(out_3)
 pool_3 = np.zeros((ker,dim_o,dim_o), dtype = 'uint8')
@@ -185,6 +234,8 @@ for x in range(0,dim_o):
         yy = y*2
         pool_3[:,x,y]= np.amax(out_3[:,xx:xx+3,yy:yy+3],(1,2))
 
+print("pool 3")
+print(pool_3[0,:,:]) # pool checking 
 pool_out_3 = open("pool_3.txt","w")
 pool_out_3_b = open("pool_3.bin","wb")
 # print(pool_3)
@@ -196,12 +247,21 @@ for x in range(0,dim_o):
 
 ########################## squeeze
 sq_in=[] # dep*dim*dim
-dep = ker*2 # TODO firs layer no ned 2
+dep = ker*2 
+if stride2_en==1:
+    dep = ker # firs layer no ned 2
+
 if pool_en == 1: # ########TODO add first layer heere
-    sq_in = np.concatenate((pool_1, pool_3), axis=0)
     dim_sq = dim_o
+    if stride2_en==1:
+        sq_in = pool_3
+    else:
+        sq_in = np.concatenate((pool_1, pool_3), axis=0)
 else:
-    sq_in = np.concatenate((out_1, out_3), axis=0)
+    if stride2_en==1:
+        sq_in=out_3
+    else:
+        sq_in = np.concatenate((out_1, out_3), axis=0)
     dim_sq = dim
 
 # print(out_1[31,:,:])
@@ -211,14 +271,16 @@ else:
 
 ########################   squ kernel
 if random == 0:
-    sq_ker_l = np.ones(sq_ker*dep, dtype='uint8').reshape((sq_ker,dep))
+    sq_ker_l = np.full(sq_ker*dep,0, dtype='uint8').reshape((sq_ker,dep))
+    sq_ker_l[0,0]=1
 else:
     # sq_ker_l = np.ones(sq_ker*dep, dtype='uint8').reshape((sq_ker,dep))
     sq_ker_l = np.random.randint(low = 0, high = 255, size = (sq_ker,dep), dtype='uint8')
 
 sq_k_1 = open("sq_ker.txt","w")
 sq_k_1_b = open("sq_ker.bin","wb")
-# print(sq_ker_l[0,:])
+print("squeeze kernel")
+print(sq_ker_l[0,:])
 dep_h = dep//2
 
 rep_no = 1
@@ -237,11 +299,13 @@ for r in range(0,rep_no):
     
 
 #######################    squ bias
-sq_bis_1 = np.random.randint(10,size=sq_ker,dtype='uint8')
-# sq_bis_1 = np.zeros(sq_ker,dtype='uint8')
+# sq_bis_1 = np.random.randint(10,size=sq_ker,dtype='uint8')
+sq_bis_1 = np.zeros(sq_ker,dtype='uint8')
 f_sq_bis = open("sq_bias.txt","w")
 f_sq_bis_b = open("sq_bias.bin","wb")
 
+print("sq_bias is")
+print(sq_bis_1)
 for x in range(0,sq_ker,8):
     lis = sq_bis_1[x:x+8]
     # lis = lis[::-1] #reverse
@@ -256,17 +320,25 @@ for k in range(0,sq_ker):
         res = np.bitwise_and(res, 0xff)
         sq_out[k,l,:,:]=res
 
-# print(sq_in[2,:,:])
-# print(sq_ker_l[0,2])
-# print(sq_out[0,2,:,:])
-
+print("squ input before add")
+inkk=0
+print("layer " + str(inkk))
+print(sq_in[inkk,:,:])
+print("kernel")
+print(sq_ker_l[0,inkk])
+print("output ")
+print(sq_out[0,inkk,:,:])
+print("single pixe")
+print(sq_out[0,:,0,0])
 sq_out = np.sum(sq_out,1,dtype='uint8') 
+print("squ after sum , before add bias")
+print(sq_out[0,:,:])
 for i in range(0,sq_ker):
     sq_out[i,:,:] = sq_out[i,:,:] + sq_bis_1[i]
 sq_out[sq_out > 127] = 0 # no need for positive
-
+print("squ after add bias")
+print(sq_out[0,:,:])
 # sq_out = np.arange(sq_ker*dim_sq*dim_sq, dtype='uint8').reshape((sq_ker,dim_sq,dim_sq)) # test ouptu
-# print(sq_out[0,:,:]);print('______')
 f_sq_out_1 = open("sq_out.txt","w")
 f_sq_out_1_b = open("sq_out.bin","wb")
 for r in range(0,dim_sq):
